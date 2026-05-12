5663e7dbd7b5efd4d14203e739399c8df4fceeff
etilorkabulo-hash <etilorkabulo@gmail.com>
2026-05-12 19:00:11 +0200

    Create LoginActivity.kt

---
+++ app/src/main/java/com/aircraftwar/pro/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.aircraftwar.pro
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.*
+
+class LoginActivity : AppCompatActivity() {
+
+    private val api = ApiService()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+
+        val nameInput = findViewById<EditText>(R.id.nameInput)
+        val registerBtn = findViewById<Button>(R.id.registerBtn)
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+
+        registerBtn.setOnClickListener {
+            val name = nameInput.text.toString()
+
+            api.register(name) { id ->
+                runOnUiThread {
+                    startGame(id)
+                }
+            }
+        }
+
+        loginBtn.setOnClickListener {
+            val name = nameInput.text.toString()
+
+            api.login(name) { id ->
+                runOnUiThread {
+                    startGame(id)
+                }
+            }
+        }
+    }
+
+    private fun startGame(playerId: Long) {
+
+        val intent = Intent(this, GameActivity::class.java)
+        intent.putExtra("playerId", playerId)
+
+        startActivity(intent)
+        finish()
+    }
+}
