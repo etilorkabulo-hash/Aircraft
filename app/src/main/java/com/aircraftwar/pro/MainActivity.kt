e2d2e02ac51400cebae407c7fdabecb2dc9234ed
etilorkabulo-hash <etilorkabulo@gmail.com>
2026-05-12 18:29:13 +0200

    Update MainActivity.kt

--- app/src/main/java/com/aircraftwar/pro/MainActivity.kt
+++ app/src/main/java/com/aircraftwar/pro/MainActivity.kt
@@ -5,15 +5,38 @@ import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val api = ApiService()
+    private var playerId: Long = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
+
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        
-        setContentView(GameView(this))
+
+        // NOM TEMPORAIRE (plus tard tu ajoutes un EditText)
+        val name = "Player_" + System.currentTimeMillis()
+
+        // 1. Créer joueur sur serveur
+        api.createPlayer(name) { id ->
+
+            runOnUiThread {
+
+                playerId = id
+
+                // 2. Créer GameView
+                val gameView = GameView(this)
+
+                // 3. Passer l’ID au jeu
+                gameView.playerId = playerId
+                gameView.api = api
+
+                setContentView(gameView)
+            }
+        }
     }
 }
